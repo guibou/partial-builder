59ae6cd8daa937bf01041595a2c494f2ddbf8205
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2022-07-29 18:36:42 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux =
+      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in { partial-builder = pkgs.haskellPackages.callCabal2nix "partial-builder" ./. { }; };
+      devShell.x86_64-linux = self.packages.x86_64-linux.partial-builder.env.overrideAttrs (old: {
+        buildInputs = old.buildInputs ++ [nixpkgs.legacyPackages.x86_64-linux.cabal-install];
+      });
+  };
+}
